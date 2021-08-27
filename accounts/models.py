a292732ff8446428b175975b7e9aa00dff67550c
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-08-27 13:39:18 +0430

    Update(accounts/models): create CustomerManager class

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,37 @@
 from django.contrib.auth.base_user import AbstractBaseUser
-from django.contrib.auth.models import PermissionsMixin, AbstractUser
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import PermissionsMixin, AbstractUser, UserManager
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
+
+
+class CustomerManager(UserManager):
+    def _create_user(self, phone_number, password, **extra_fields):
+
+        if not phone_number:
+            raise ValueError('The given phone must be set')
+
+        user = self.model(phone_number=phone_number, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, phone_number, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(phone_number, password, **extra_fields)
+
+    def create_superuser(self, phone_number, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(phone_number, password, **extra_fields)
 
 
 class Customer(AbstractBaseUser, PermissionsMixin):
