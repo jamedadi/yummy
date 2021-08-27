fba2625d2376c392a412c0b3588adf07eac53441
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-08-27 17:20:55 +0430

    Update(cart/models): add null, blank True for invoic of cart

--- cart/models.py
+++ cart/models.py
@@ -10,7 +10,8 @@ class Cart(BaseModel):
     customer = models.ForeignKey(Customer, verbose_name=_('customer'), null=True, blank=True, related_name='carts',
                                  on_delete=models.CASCADE)
     is_paid = models.BooleanField(default=False, verbose_name=_('is paid'))
-    invoice = models.OneToOneField(Invoice, verbose_name=_('invoice'), related_name='cart', on_delete=models.PROTECT)
+    invoice = models.OneToOneField(Invoice, verbose_name=_('invoice'), related_name='cart', on_delete=models.PROTECT,
+                                   null=True, blank=True)
 
     def __str__(self):
         return f"{self.customer} - {'Paid' if self.is_paid else 'Not paid'}"
