be5147357e841ca85aa9d183d21661882c731105
MrMohammadY <m.yazdani000@gmail.com>
2021-08-27 20:31:22 +0430

    Add(order/model): create order models

--- order/models.py
+++ order/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from library.models import BaseModel
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Order(BaseModel):
+    # TODO-1: add foreign key to invoice here
+    # TODO-2: add status here
+    is_delivered = models.BooleanField(verbose_name=_('is delivered'), default=False)
