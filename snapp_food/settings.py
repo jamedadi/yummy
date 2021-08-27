0ea8c145acf6c9461af3a862906a1ad3e3c93c3a
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-08-27 14:01:15 +0430

    fix(settings): fix import error for local_settings

--- snapp_food/settings.py
+++ snapp_food/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from local_settings import *
+from snapp_food.local_settings import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
