21e8ba806e3b663b0e9dad71147b4aed4fc42122
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-08-27 16:23:47 +0430

    Add(item/admin): register Item model to admin panel

--- item/admin.py
+++ item/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from item.models import Item
+
+
+@admin.register(Item)
+class Item(admin.ModelAdmin):
+    list_display = ('name', 'upc', 'price', 'stock', 'available')
+    list_editable = ('available',)
+    list_filter = ('available', 'created_time')
+    search_fields = ('upc', 'name')
