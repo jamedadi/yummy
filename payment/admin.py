b4681be7b3e830abec9efb4556080e681a562859
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-08-27 22:14:06 +0430

    Update(payment/admin): add created_time filter for invoice, payment admin

--- payment/admin.py
+++ payment/admin.py
@@ -6,12 +6,12 @@ from payment.models import Invoice, Payment
 @admin.register(Invoice)
 class InvoiceAdmin(admin.ModelAdmin):
     list_display = ('customer', 'price', 'is_paid')
-    list_filter = ('is_paid',)
+    list_filter = ('is_paid', 'crated_time')
     search_fields = ('customer__phone_number',)
 
 
 @admin.register(Payment)
 class PaymentAdmin(admin.ModelAdmin):
     list_display = ('uuid', 'price', 'is_paid')
-    list_filter = ('is_paid',)
+    list_filter = ('is_paid', 'created_time')
     search_fields = ('uuid',)
