e53bbd1b5ffb313a42dbc51b8619c16e84c97c52
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-08-27 17:04:52 +0430

    Add(payment/models): add Invoice model

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils.translation import gettext as _
+from accounts.models import Customer
+from library.models import BaseModel
 
-# Create your models here.
+
+class Invoice(BaseModel):
+    customer = models.ForeignKey(Customer, verbose_name=_('customer'), related_name='invoices',
+                                 on_delete=models.SET_NULL)
+    price = models.IntegerField(verbose_name=_('price'))
+    is_paid = models.BooleanField(verbose_name=_('is paid'), default=False)
+
+    #  TODO: Address must be created for relation
+
+    class Meta:
+        verbose_name = _('invoice')
+        verbose_name_plural = _('invoices')
+        db_table = 'invoice'
