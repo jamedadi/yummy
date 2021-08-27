994944c65218f5de10100be0ee56281de756d576
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-08-27 17:15:47 +0430

    Add(payment/admin): register Invoice, Admin to admin panel

--- payment/admin.py
+++ payment/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from payment.models import Invoice, Payment
+
+
+@admin.register(Invoice)
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'price', 'is_paid')
+    list_filter = ('is_paid',)
+    search_fields = ('customer__phone_number',)
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('uuid', 'price', 'is_paid')
+    list_filter = ('is_paid',)
+    search_fields = ('uuid',)
