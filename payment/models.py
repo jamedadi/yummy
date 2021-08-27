921366bfc2ee4d9aff86d29c4ae7945789fe4ac4
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-08-27 17:22:25 +0430

    Update(payment/models): add null, blank True for customer of invoice

--- payment/models.py
+++ payment/models.py
@@ -6,7 +6,7 @@ from library.models import BaseModel
 
 class Invoice(BaseModel):
     customer = models.ForeignKey(Customer, verbose_name=_('customer'), related_name='invoices',
-                                 on_delete=models.SET_NULL)
+                                 on_delete=models.SET_NULL, null=True, blank=True)
     price = models.IntegerField(verbose_name=_('price'))
     is_paid = models.BooleanField(verbose_name=_('is paid'), default=False)
 
