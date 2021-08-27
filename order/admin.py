001afe029a2d8a0524d2bb42fecce16725556de0
MrMohammadY <m.yazdani000@gmail.com>
2021-08-27 20:34:49 +0430

    Add(order/admin): create order admin models

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    # TODO-1: after done todos the model order, add the fields here
+    pass
