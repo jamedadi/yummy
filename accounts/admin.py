74c9e8c1df3f22b36ac820f38ff57645db2645e7
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-08-27 15:30:53 +0430

    Update(accounts/admin): register accounts models to admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext as _
 
-# Register your models here.
+from accounts.models import User, Customer, ServiceProvider
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    pass
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('phone_number', 'first_name', 'last_name', 'date_joined', 'is_active')
+    list_filter = ('is_active', 'date_joined')
+    search_fields = ('phone_number',)
+
+
+@admin.register(ServiceProvider)
+class ServiceProviderAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ('phone_number',)
+    search_fields = UserAdmin.search_fields + ('phone_number',)
+    fieldsets = UserAdmin.fieldsets + (
+        (_("Phone number"), {'fields': ('phone_number',)}),
+    )
