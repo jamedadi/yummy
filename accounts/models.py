3b9cd9762d8580dfdaa849fadef409581363f4f4
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-08-27 13:28:10 +0430

    Update(accounts/models): create customer model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Customer(AbstractBaseUser, PermissionsMixin):
+    phone_number = models.CharField(max_length=13, verbose_name=_('phone number'), unique=True)
+    first_name = models.CharField(_('first name'), max_length=150, blank=True)
+    last_name = models.CharField(_('last name'), max_length=150, blank=True)
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_('Designates whether the user can log into this admin site.'),
+    )
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
+    )
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = []
+
+    @property
+    def full_name(self):
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    class Meta:
+        verbose_name = _('customer')
+        verbose_name_plural = _('customers')
+        db_table = 'customer'
