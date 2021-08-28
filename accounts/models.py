c1ee2b6a047f18e835ddf175c85217c9372cfa49
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-08-28 16:26:02 +0430

    Typo(accounts/models): fix a typo in error message

--- accounts/models.py
+++ accounts/models.py
@@ -19,7 +19,7 @@ class CustomerManager(UserManager):
     def _create_user(self, phone_number, password, **extra_fields):
 
         if not phone_number:
-            raise ValueError('The given phone must be set')
+            raise ValueError('The given phone_number must be set')
 
         user = self.model(phone_number=phone_number, **extra_fields)
         user.password = make_password(password)
