91fd43e2e31d5f5d3fdc2f41aef765a82bc94aae
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-08-28 14:29:30 +0430

    Update(item/admin): add service, category to list_display

--- item/admin.py
+++ item/admin.py
@@ -5,7 +5,7 @@ from item.models import Item
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
-    list_display = ('name', 'upc', 'price', 'stock', 'available')
+    list_display = ('name', 'upc', 'service', 'category', 'price', 'stock', 'available')
     list_editable = ('available',)
     list_filter = ('available', 'created_time')
     search_fields = ('upc', 'name')
