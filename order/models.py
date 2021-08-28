991ee51b1801fc4ba2a79c8555c4cf007e2a5970
MrMohammadY <m.yazdani000@gmail.com>
2021-08-28 17:49:53 +0430

    Update(order/models): add invoice, status field in model

--- order/models.py
+++ order/models.py
@@ -2,8 +2,17 @@ from django.db import models
 from library.models import BaseModel
 from django.utils.translation import ugettext_lazy as _
 
+from payment.models import Invoice
+
 
 class Order(BaseModel):
-    # TODO-1: add foreign key to invoice here
-    # TODO-2: add status here
+    PREPARING_FOOD = 0
+    SENDING = 1
+
+    STATUS = (
+        (PREPARING_FOOD, _('preparing food')),
+        (PREPARING_FOOD, _('sending')),
+    )
+    invoice = models.ForeignKey(Invoice, verbose_name=_('invoice'), related_name='orders', on_delete=models.PROTECT)
+    status = models.PositiveSmallIntegerField(verbose_name=_('status'), choices=STATUS)
     is_delivered = models.BooleanField(verbose_name=_('is delivered'), default=False)
