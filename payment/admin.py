9000548bc0faf33d74d0d09003b4961e5192feb9
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-08-28 14:31:16 +0430

    Update(payment/admin): add address to list_display of InvoiceAdmin

--- payment/admin.py
+++ payment/admin.py
@@ -5,7 +5,7 @@ from payment.models import Invoice, Payment
 
 @admin.register(Invoice)
 class InvoiceAdmin(admin.ModelAdmin):
-    list_display = ('customer', 'price', 'is_paid')
+    list_display = ('customer', 'price', 'is_paid', 'address')
     list_filter = ('is_paid', 'crated_time')
     search_fields = ('customer__phone_number',)
 
