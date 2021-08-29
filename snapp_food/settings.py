4b53fc5dd6a9a8b8c4bd47ddfbc4e7085dbbb407
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-08-29 13:00:54 +0430

    Update(settings): add PhoneNumberPasswordBackend to settings

--- snapp_food/settings.py
+++ snapp_food/settings.py
@@ -124,3 +124,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'accounts.authenticate.PhoneNumberPasswordBackend'
+]
