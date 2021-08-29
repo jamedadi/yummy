6bd35318e1c267fcab66fc89b3d77d830e4eeb54
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-08-29 21:56:16 +0430

    Add(accounts/forms): create CustomerPasswordSetForm class

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,11 @@
 from django import forms
+from django.contrib.auth import password_validation
+from django.core.exceptions import ValidationError
 
 from django.core.validators import int_list_validator
 from django.utils.translation import gettext_lazy as _
+
+from accounts.models import Customer
 
 
 class CustomerLoginRegisterForm(forms.Form):
@@ -35,3 +39,39 @@ class CustomerPasswordForm(forms.Form):
             }
         )
     )
+
+
+class CustomerPasswordSetForm(forms.ModelForm):
+    password = forms.CharField(
+        label=_("Password"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+    password2 = forms.CharField(
+        label=_("Password confirmation"),
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
+        strip=False,
+        help_text=_("Enter the same password as before, for verification."),
+    )
+
+    class Meta:
+        model = Customer
+        fields = ('password',)
+
+    def clean_password2(self):
+        password = self.cleaned_data.get("password")
+        password2 = self.cleaned_data.get("password2")
+        if password and password2 and password != password2:
+            raise ValidationError(
+                self.error_messages['password_mismatch'],
+                code='password_mismatch',
+            )
+        return password2
+
+    def save(self, commit=True):
+        customer = super().save(commit=False)
+        customer.set_password(self.cleaned_data["password1"])
+        if commit:
+            customer.save()
+        return customer
