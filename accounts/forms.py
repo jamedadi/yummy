2013be3874013e0e1dc71821beeb056d04c01811
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-08-29 15:38:59 +0430

    Add(account/forms): create CustomerLoginRegisterForm class

---
+++ accounts/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.core.validators import int_list_validator
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomerLoginRegisterForm(forms.Form):
+    phone_number = forms.CharField(max_length=10,
+                                   min_length=10,
+                                   validators=[int_list_validator(message=_('only digits are accepted'))],
+                                   error_messages={'min_length': _('phone number must have 10 digits')},
+                                   widget=forms.TextInput(
+                                       attrs={'class': 'form-control', 'placeholder': 'phone number'})
+                                   )
