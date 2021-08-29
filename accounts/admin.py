a993f0fb6934316e2b34654a0dc2137b91b84570
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-08-29 22:59:45 +0430

    Update(accounts/admin): override save_model method of CustomerAdmin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+from django.contrib.auth.hashers import make_password
 
 from accounts.models import Customer, ServiceProvider
 
@@ -9,6 +9,10 @@ class CustomerAdmin(admin.ModelAdmin):
     list_display = ('phone_number', 'first_name', 'last_name', 'date_joined', 'is_active')
     list_filter = ('is_active', 'date_joined')
     search_fields = ('phone_number',)
+
+    def save_model(self, request, obj, form, change):
+        obj.password = make_password(form.cleaned_data['password'])
+        return super().save_model(request, obj, form, change)
 
 
 @admin.register(ServiceProvider)
