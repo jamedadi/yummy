a16945df80ace545e78248de3dea827044589f59
MrMohammadY <m.yazdani000@gmail.com>
2021-08-29 23:03:40 +0430

    Fix(aacounts/urls): fix the name view url

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ServiceProviderRegistrationView, ServiceProviderLoginView, UserLogoutView, ServiceProviderProfileView
+from .views import ServiceProviderRegistrationView, ServiceProviderLoginView, LogoutView, ServiceProviderProfileView
 
 app_name = 'accounts'
 urlpatterns = [
@@ -8,5 +8,5 @@ urlpatterns = [
     ),
     path('serviceprovider/login/', ServiceProviderLoginView.as_view(), name='service-provider-login'),
     path('serviceprovider/profile/', ServiceProviderProfileView.as_view(), name='service-provider-profile'),
-    path('user/logout/', UserLogoutView.as_view(), name='user-logout')
+    path('user/logout/', LogoutView.as_view(), name='user-logout')
 ]
