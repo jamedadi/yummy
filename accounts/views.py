bde2a2d5151b5076aafd5dce2ab8140782ab3cb4
MrMohammadY <m.yazdani000@gmail.com>
2021-08-29 16:49:49 +0430

    Update(accounts/views): update success url field in service provider registeration view

--- accounts/views.py
+++ accounts/views.py
@@ -7,7 +7,7 @@ from .forms import ServiceProviderRegistrationForm
 class ServiceProviderRegistrationView(FormView):
     form_class = ServiceProviderRegistrationForm
     template_name = 'accounts/service_provider_registration.html'
-    success_url = reverse_lazy('')
+    success_url = reverse_lazy('accounts:service-provider-login')
 
     def form_valid(self, form):
         instance = form.save(commit=False)
