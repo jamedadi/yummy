fcc79548ae7d979548b3270883520e197762138a
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-08-29 16:20:22 +0430

    Add(accounts/views): add CustomerLoginRegisterView class view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.views.generic import FormView
+from accounts.forms import CustomerLoginRegisterForm
+from accounts.models import Customer
 
-# Create your views here.
+
+class CustomerLoginRegisterView(FormView):
+    form_class = CustomerLoginRegisterForm
+    template_name = None
+
+    def form_valid(self, form):
+        try:
+            Customer.objects.get(phone_number=form.changed_data['phone_number'])
+        except Customer.DoesNotExist:
+            self.success_url = None
+        else:
+            self.success_url = None
+
+        return super().form_valid(form)
