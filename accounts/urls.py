d41b58d1b464108cacc4e8e72cef309f6017ef0f
MrMohammadY <m.yazdani000@gmail.com>
2021-08-29 16:57:21 +0430

    Add(accounts/urls): add service provider logout url

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
         'serviceprovider/registration/', ServiceProviderRegistrationView.as_view(), name='service-provider-registration'
     ),
     path('serviceprovider/login/', ServiceProviderLoginView.as_view(), name='service-provider-login'),
+    path('serviceprovider/logout/', name='service-provider-logout')
 ]
