f0188f0aaf3799fde97ce4f09315aeef9427812c
MrMohammadY <m.yazdani000@gmail.com>
2021-08-29 14:02:12 +0430

    Update(settings): add authentication to auth_backends

--- snapp_food/settings.py
+++ snapp_food/settings.py
@@ -124,3 +124,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'accounts.authenticate.ServiceProviderAuthentication',
+)
