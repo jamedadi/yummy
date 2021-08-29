3f6bfbc2f23e093cac2800db543269dc77a00f52
MrMohammadY <m.yazdani000@gmail.com>
2021-08-29 18:53:32 +0430

    Update(accounts/urls): update user logout url

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ServiceProviderRegistrationView, ServiceProviderLoginView
+from .views import ServiceProviderRegistrationView, ServiceProviderLoginView, UserLogoutView
 
 app_name = 'accounts'
 urlpatterns = [
@@ -7,5 +7,5 @@ urlpatterns = [
         'serviceprovider/registration/', ServiceProviderRegistrationView.as_view(), name='service-provider-registration'
     ),
     path('serviceprovider/login/', ServiceProviderLoginView.as_view(), name='service-provider-login'),
-    path('serviceprovider/logout/', name='service-provider-logout')
+    path('user/logout/', UserLogoutView.as_view(), name='user-logout')
 ]
