40bf5c70bf426baf62b669adff6c6b86b614d227
MrMohammadY <m.yazdani000@gmail.com>
2021-08-30 13:46:43 +0430

    Add(address/views): create customer address create view

--- address/views.py
+++ address/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from address.forms import CustomerAddressCreateForm
+from address.models import CustomerAddress
+
+
+class CustomerAddressCreateView(CreateView):
+    model = CustomerAddress
+    form_class = CustomerAddressCreateForm
+    template_name = 'address/create.html'
+    success_url = reverse_lazy('accounts:customer-profile')
+
+    def form_valid(self, form):
+        instance = form.save(commit=False)
+        instance.user = self.request.user
+        instance.save()
+        return super().form_valid(form)
