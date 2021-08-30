2ff964105018ea8b714f55792fad9bd808ed576d
MrMohammadY <m.yazdani000@gmail.com>
2021-08-30 13:47:22 +0430

    Add(address/urls): create customer address url

--- address/urls.py
+++ address/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
+from .views import CustomerAddressCreateView
+
 app_name = 'address'
 
 urlpatterns = [
+    path('customer/create', CustomerAddressCreateView.as_view(), name='customer-address-create')
 ]
