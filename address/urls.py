e9024093c22edc34061bddfec577e7ab8ac2d17f
MrMohammadY <m.yazdani000@gmail.com>
2021-08-30 17:48:46 +0430

    Update(address/urls): create a new url for update and delete address

--- address/urls.py
+++ address/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import CustomerAddressCreateView
+from .views import CustomerAddressCreateView, CustomerAddressUpdateView, CustomerAddressDeleteView
 
 app_name = 'address'
 
 urlpatterns = [
-    path('customer/create', CustomerAddressCreateView.as_view(), name='customer-address-create')
+    path('customer/create', CustomerAddressCreateView.as_view(), name='customer-address-create'),
+    path('customer/update/<int:pk>', CustomerAddressUpdateView.as_view(), name='customer-address-update'),
+    path('customer/delete/<int:pk>', CustomerAddressDeleteView.as_view(), name='customer-address-delete'),
 ]
