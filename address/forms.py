6004c4545404b3942e8337b304196e4cab13f052
MrMohammadY <m.yazdani000@gmail.com>
2021-08-30 13:45:48 +0430

    Add(address/forms): create customer address create form

---
+++ address/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+
+from address.models import CustomerAddress
+
+
+class CustomerAddressCreateForm(forms.ModelForm):
+    class Meta:
+        model = CustomerAddress
+        fields = ('state', 'city', 'area', 'street', 'alley', 'floor', 'plaque')
+        widgets = {
+            'state': forms.Select(attrs={'class': 'form-control'}),
+            'city': forms.Select(attrs={'class': 'form-control'}),
+            'area': forms.Select(attrs={'class': 'form-control'}),
+            'street': forms.TextInput(attrs={'class': 'form-control'}),
+            'alley': forms.TextInput(attrs={'class': 'form-control'}),
+            'floor': forms.NumberInput(attrs={'class': 'form-control'}),
+            'plaque': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
