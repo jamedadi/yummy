89986684bf72e4de0fc9ce807007e8f8be51681e
MrMohammadY <m.yazdani000@gmail.com>
2021-08-30 19:35:20 +0430

    Add(address/views): create service address update view and refactor service address create view

--- address/views.py
+++ address/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import UserPassesTestMixin
 from django.db import transaction
 from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
@@ -57,3 +58,17 @@ class ServiceAddressCreateView(CreateView):
             self.service.address = instance
             self.service.save()
             return super().form_valid(form)
+
+
+class ServiceAddressUpdateView(UpdateView):
+    model = ServiceAddress
+    form_class = ServiceAddressCreateUpdateForm
+    template_name = 'address/create_update_form.html'
+    success_url = reverse_lazy('accounts:service-provider-profile')
+
+    def form_valid(self, form):
+        with transaction.atomic():
+            instance = form.save(commit=False)
+            if self.request.user == instance.services.last().service_provider:
+                instance.save()
+        return super().form_valid(form)
