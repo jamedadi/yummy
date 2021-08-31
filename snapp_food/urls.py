14cdba7ef84435e30dd24621d8646dea2680747d
MrMohammadY <m.yazdani000@gmail.com>
2021-08-31 19:23:08 +0430

    Add(urls): add home path url for test

--- snapp_food/urls.py
+++ snapp_food/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', TemplateView.as_view(template_name='base.html'), name='home'),
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('address/', include('address.urls', namespace='address')),
 ]
