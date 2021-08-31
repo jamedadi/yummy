8c811f707f8033f141476a615d65ff4a685f0a47
MrMohammadY <m.yazdani000@gmail.com>
2021-08-31 16:31:19 +0430

    Update(address/views): use custom passess test in all views

--- address/views.py
+++ address/views.py
@@ -1,11 +1,14 @@
-from django.contrib.auth.mixins import UserPassesTestMixin
+from django.contrib.auth.decorators import login_required
 from django.db import transaction
 from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView, DeleteView
 
+from accounts.models import Customer, ServiceProvider
 from address.forms import CustomerAddressCreateUpdateForm, ServiceAddressCreateUpdateForm
 from address.models import CustomerAddress, ServiceAddress
+from library.utils import CustomUserPasses
 from service.models import Service
 
 
@@ -15,9 +18,14 @@ class BaseAddress:
     template_name = 'address/create_update_form.html'
 
 
+@method_decorator(login_required(login_url=reverse_lazy('accounts:customer-login-register')), name='dispatch')
 class CustomerAddressCreateView(BaseAddress, CreateView):
     success_url = reverse_lazy('accounts:customer-profile')
 
+    def test_func(self):
+        if not isinstance(self.request.user, Customer):
+            return False
+
     def form_valid(self, form):
         instance = form.save(commit=False)
         instance.user = self.request.user
@@ -25,9 +33,18 @@ class CustomerAddressCreateView(BaseAddress, CreateView):
         return super().form_valid(form)
 
 
+@method_decorator(login_required(login_url=reverse_lazy('accounts:customer-login-register')), name='dispatch')
 class CustomerAddressUpdateView(BaseAddress, UpdateView):
     success_url = reverse_lazy('accounts:customer-profile')
 
+    def test_func(self):
+        if not isinstance(self.request.user, Customer):
+            return False
+
+        obj = self.get_object()
+        if obj.services.service_provider != self.request.user:
+            return False
+
     def form_valid(self, form):
         instance = form.save(commit=False)
         instance.user = self.request.user
@@ -35,13 +52,23 @@ class CustomerAddressUpdateView(BaseAddress, UpdateView):
         return super().form_valid(form)
 
 
-class CustomerAddressDeleteView(DeleteView):
+@method_decorator(login_required(login_url=reverse_lazy('accounts:customer-login-register')), name='dispatch')
+class CustomerAddressDeleteView(CustomUserPasses, DeleteView):
     model = CustomerAddress
     template_name = 'address/delete_form.html'
     success_url = reverse_lazy('accounts:customer-profile')
 
+    def test_func(self):
+        if not isinstance(self.request.user, Customer):
+            return False
 
-class ServiceAddressCreateView(CreateView):
+        obj = self.get_object()
+        if obj.customer_user != self.request.user:
+            return False
+
+
+@method_decorator(login_required(login_url=reverse_lazy('accounts:service-provider-login')), name='dispatch')
+class ServiceAddressCreateView(CustomUserPasses, CreateView):
     model = ServiceAddress
     form_class = ServiceAddressCreateUpdateForm
     template_name = 'address/create_update_form.html'
@@ -52,6 +79,14 @@ class ServiceAddressCreateView(CreateView):
         super().setup(request, *args, **kwargs)
         self.service = get_object_or_404(Service, id=self.kwargs['service_pk'])
 
+    def test_func(self):
+        if not isinstance(self.request.user, ServiceProvider):
+            return False
+        if self.service.service_provider != self.request.user:
+            return False
+        if self.service.address:
+            return False, True, reverse_lazy('accounts:service-provider-profile')
+
     def form_valid(self, form):
         with transaction.atomic():
             instance = form.save()
@@ -60,15 +95,17 @@ class ServiceAddressCreateView(CreateView):
             return super().form_valid(form)
 
 
-class ServiceAddressUpdateView(UpdateView):
+@method_decorator(login_required(login_url=reverse_lazy('accounts:service-provider-login')), name='dispatch')
+class ServiceAddressUpdateView(CustomUserPasses, UpdateView):
     model = ServiceAddress
     form_class = ServiceAddressCreateUpdateForm
     template_name = 'address/create_update_form.html'
     success_url = reverse_lazy('accounts:service-provider-profile')
 
-    def form_valid(self, form):
-        with transaction.atomic():
-            instance = form.save(commit=False)
-            if self.request.user == instance.services.last().service_provider:
-                instance.save()
-        return super().form_valid(form)
+    def test_func(self):
+        if not isinstance(self.request.user, ServiceProvider):
+            return False
+
+        obj = self.get_object()
+        if obj.services.service_provider != self.request.user:
+            return False
