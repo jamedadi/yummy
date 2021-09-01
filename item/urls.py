e93544d2c0badd5a4233e86e8d97816dfc82c4bb
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-09-01 10:51:40 +0430

    Update(item/urls): add update url

--- item/urls.py
+++ item/urls.py
@@ -1,5 +1,8 @@
+from django.urls import path
+from item.views import ItemUpdateView
+
 app_name = 'item'
 
 urlpatterns = [
-
+    path('update/<int:pk>/', ItemUpdateView.as_view(), name='update'),
 ]
