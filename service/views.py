5b311cc7554f402983b79fbd9a7f6e5f8078a885
MrMohammadY <m.yazdani000@gmail.com>
2021-09-01 12:49:42 +0430

    Add(service/views): create service update view

--- service/views.py
+++ service/views.py
@@ -1,17 +1,17 @@
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, UpdateView
 
 from accounts.utils import IsServiceProvider
-from service.forms import ServiceCreateForm
+from service.forms import ServiceCreateUpdateForm
 from service.models import Service
 
 
 @method_decorator(login_required(login_url=reverse_lazy('accounts:service-provider-login')), name='dispatch')
 class ServiceCreateView(IsServiceProvider, CreateView):
     model = Service
-    form_class = ServiceCreateForm
+    form_class = ServiceCreateUpdateForm
     template_name = 'service/create_form.html'
     success_url = reverse_lazy('accounts:service-provider-profile')
 
@@ -20,6 +20,19 @@ class ServiceCreateView(IsServiceProvider, CreateView):
         instance.service_provider = self.request.user
         instance.save()
         return super().form_valid(form)
+
+
+@method_decorator(login_required(login_url=reverse_lazy('accounts:service-provider-login')), name='dispatch')
+class ServiceUpdateView(IsServiceProvider, UpdateView):
+    model = Service
+    form_class = ServiceCreateUpdateForm
+    template_name = 'service/create_form.html'
+    success_url = reverse_lazy('service:service-list')
+
+    def test_func(self):
+        result = super().test_func()
+        obj = self.get_object()
+        return obj.service_provider == self.request.user and result
 
 
 @method_decorator(login_required(login_url=reverse_lazy('accounts:service-provider-login')), name='dispatch')
