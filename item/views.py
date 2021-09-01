719370ba8cf15cdeb7eacdb8e592c4859a7064af
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-09-01 10:11:06 +0430

    Update(item/views): add test_func for CreatItemView

--- item/views.py
+++ item/views.py
@@ -1,18 +1,36 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import FormView
 
+from accounts.utils import IsServiceProvider
 from item.forms import ItemCreateForm
 from item.models import Item
+from service.models import Service, ServiceCategory
 
 
-class CreatItemView(FormView):
+@method_decorator(login_required(login_url=reverse_lazy('accounts:service-provider-login')), name='dispatch')
+class CreatItemView(IsServiceProvider, FormView):
     model = Item
     form_class = ItemCreateForm
     template_name = 'item/create_form.html'
 
+    def setup(self, request, *args, **kwargs):
+        super().setup(request, *args, **kwargs)
+        self.kwargs['service'] = get_object_or_404(Service, id=self.kwargs['service_pk'])
+        self.kwargs['category'] = get_object_or_404(ServiceCategory, id=self.kwargs['category_pk'])
+
     def form_valid(self, form):
         item = form.save(commit=False)
-        item.service_id = form.cleaned_data['service_id']
-        item.category_id = form.cleaned_data['category_id']
+        item.service = self.kwargs['service']
+        item.category = self.kwargs['category']
         item.line.quantity += form.cleaned_data['quantity']
         item.save()
         return super().form_valid(form)
+
+    def test_func(self):
+        result = super().test_func()
+        service_check = self.kwargs['service'].service_provider == self.request.user
+        category_check = self.kwargs['category'].service == self.kwargs['service']
+        return result and service_check and category_check
