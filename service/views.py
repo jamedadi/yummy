ab67b6e662fcb7419949a6d99e2d1125933e5715
MrMohammadY <m.yazdani000@gmail.com>
2021-09-01 12:03:23 +0430

    Add(service/views): create service create view

--- service/views.py
+++ service/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView
 
-# Create your views here.
+from accounts.utils import IsServiceProvider
+from service.forms import ServiceCreateForm
+from service.models import Service
+
+
+@method_decorator(login_required(login_url=reverse_lazy('accounts:service-provider-login')), name='dispatch')
+class ServiceCreateView(IsServiceProvider, CreateView):
+    model = Service
+    form_class = ServiceCreateForm
+    template_name = 'service/create_form.html'
+    success_url = reverse_lazy('accounts:service-provider-profile')
+
+    def form_valid(self, form):
+        instance = form.save(commit=False)
+        instance.service_provider = self.request.user
+        instance.save()
+        return super().form_valid(form)
