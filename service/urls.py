1c713c4066a9e179683c6cd480ebfc2d4df7f758
MrMohammadY <m.yazdani000@gmail.com>
2021-09-01 12:53:23 +0430

    Add(service/urls): create service update url

--- service/urls.py
+++ service/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import ServiceCreateView, ServiceListView
+from .views import ServiceCreateView, ServiceUpdateView, ServiceListView
 
 app_name = 'service'
 
 urlpatterns = (
     path('create/', ServiceCreateView.as_view(), name='service-create'),
+    path('update/<int:pk>', ServiceUpdateView.as_view(), name='service-update'),
     path('list/', ServiceListView.as_view(), name='service-list'),
 )
