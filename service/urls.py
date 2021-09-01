0e93c19ca12068f85f7a1fb3ae3b067fb84f5ff5
MrMohammadY <m.yazdani000@gmail.com>
2021-09-01 13:08:24 +0430

    Add(service/urls): create service delete url

--- service/urls.py
+++ service/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import ServiceCreateView, ServiceUpdateView, ServiceListView
+from .views import ServiceCreateView, ServiceUpdateView, ServiceDeleteView, ServiceListView
 
 app_name = 'service'
 
 urlpatterns = (
     path('create/', ServiceCreateView.as_view(), name='service-create'),
     path('update/<int:pk>', ServiceUpdateView.as_view(), name='service-update'),
+    path('delete/<int:pk>', ServiceDeleteView.as_view(), name='service-delete'),
     path('list/', ServiceListView.as_view(), name='service-list'),
 )
