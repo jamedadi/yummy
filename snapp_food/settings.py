2ca40c4489b1236bcf9a395101a99aa8d88b090a
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-09-02 11:30:30 +0430

    Add(snapp_food/urls, settings): add static and media condigs in settings and add static url for testing in local

--- snapp_food/settings.py
+++ snapp_food/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- snapp_food/urls.py
+++ snapp_food/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
@@ -23,4 +25,4 @@ urlpatterns = [
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('address/', include('address.urls', namespace='address')),
     path('item/', include('item.urls', namespace='item'))
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
