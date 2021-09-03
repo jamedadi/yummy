c7c916720e7a3b59eddc95c03d0e3e13b3b0efe2
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-09-03 16:09:01 +0430

    Update(item/views, urls): change the url of item-list and the view(category_pk removed from url)

--- item/urls.py
+++ item/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     path('detail/<int:pk>/', ItemDetailView.as_view(), name='detail'),
     path('detail-serviceprovider/<int:pk>/', ServiceProviderItemDetailView.as_view(), name='detail-service-provider'),
     path('delete/<int:pk>/', ItemDeleteView.as_view(), name='delete'),
-    path('<int:service_pk>/<int:category_pk>/list', ItemListView.as_view(), name='list'),
+    path('<int:service_pk>/list/', ItemListView.as_view(), name='list'),
     path(
-        'serviceprovider/<int:service_pk>/<int:category_pk>/list', ServiceCategoryItemListView.as_view(),
+        'serviceprovider/<int:service_pk>/<int:category_pk>/list/', ServiceCategoryItemListView.as_view(),
         name='service-provider-list'),
 ]
