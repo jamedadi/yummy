5d25732b7b2dba8fa24405931dbcaa38b51d364e
MrMohammadY <m.yazdani000@gmail.com>
2021-09-03 12:54:38 +0430

    Update(urls): add media root and url in project

--- snapp_food/urls.py
+++ snapp_food/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
@@ -24,3 +26,6 @@ urlpatterns = [
     path('service/', include('service.urls', namespace='service')),
     path('address/', include('address.urls', namespace='address')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
