8f4a42817889173913e55ee6386c6d5929e1e798
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-09-07 19:00:11 +0430

    Update(cart/urls): add cart-line-decrease url to cart.urls

--- cart/urls.py
+++ cart/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import AddToCartView, CartLineDeleteView
+from cart.views import AddToCartView, CartLineDeleteView, CartLineDecreaseView
 
 app_name = 'cart'
 
 urlpatterns = [
     path('addtocart/<int:item_pk>/', AddToCartView.as_view(), name='add-to-cart'),
     path('cartline/delete/<int:pk>/', CartLineDeleteView.as_view(), name='cart-line-delete'),
+    path('cartline/decrease/<int:pk>/', CartLineDecreaseView.as_view(), name='cart-line-decrease'),
 ]
