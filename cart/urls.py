befc4b37ad1ef0c3c31b1e8cb15d22baebe322b8
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-09-07 18:54:27 +0430

    Update(cart/urls): add cart-line-delete url to cart.urls

--- cart/urls.py
+++ cart/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import AddToCartView
+from .views import AddToCartView, CartLineDeleteView
 
 app_name = 'cart'
 
 urlpatterns = [
     path('addtocart/<int:item_pk>/', AddToCartView.as_view(), name='add-to-cart'),
+    path('cartline/delete/<int:pk>/', CartLineDeleteView.as_view(), name='cart-line-delete'),
 ]
