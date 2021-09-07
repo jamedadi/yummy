65131032ed60912a3498934a7a0969f9a07b5b3a
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-09-07 18:56:18 +0430

    Add(cart/views): create CartLineDecreaseView view

--- cart/views.py
+++ cart/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.decorators import login_required
+from django.db import transaction
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.decorators.http import require_http_methods
-from django.views.generic import View, DeleteView
+from django.views.generic import View, DeleteView, DetailView
 
 from cart.models import Cart, CartLine
 from item.models import Item
@@ -31,3 +32,21 @@ class CartLineDeleteView(DeleteView):
 
     def get_success_url(self):
         return reverse_lazy('item:list', kwargs={'service_pk': self.object.item.service.pk})
+
+
+@method_decorator(require_http_methods(('POST',)), name='dispatch')
+@method_decorator(login_required(login_url=reverse_lazy('accounts:customer-login-register')), name='dispatch')
+class CartLineDecreaseView(DetailView):
+    model = CartLine
+
+    def get_success_url(self):
+        return reverse_lazy('item:list', kwargs={'service_pk': self.object.item.service.pk})
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+
+        with transaction.atomic():
+            if self.object.quantity >= 2:
+                self.object.quantity += -1
+                self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
