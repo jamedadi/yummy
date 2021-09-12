071e8dab75af3473c4edefbcdc294ad7268d680a
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-09-12 16:51:56 +0430

    Add(gateway/models): create Gateway model

--- gateway/models.py
+++ gateway/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+from library.models import BaseModel
+
+
+class Gateway(BaseModel):
+    FUNCTION_ZARRINPAL = 'zarrinpal'
+    FUNCTION_SAMAN = 'saman'
+    GATEWAY_FUNCTIONS_CHOICES = (
+        (FUNCTION_ZARRINPAL, _('Zarrinpal')),
+        (FUNCTION_SAMAN, _('Saman'))
+    )
+
+    title = models.CharField(max_length=100, verbose_name=_('title'))
+    gateway_request_url = models.CharField(max_length=150, verbose_name=_('request url'), blank=True)
+    gateway_verify_url = models.CharField(max_length=150, verbose_name=_('verify url'), blank=True)
+    gateway_code = models.CharField(max_length=20, verbose_name=_('gateway code'), choices=GATEWAY_FUNCTIONS_CHOICES)
+    is_enable = models.BooleanField(verbose_name=_('is enable'), default=True)
+    auth_data = models.TextField(verbose_name=_('auth data'), blank=True)
