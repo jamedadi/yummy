bf6d92c6309878d50d7a7b6d067fe504175faa74
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-09-12 16:41:23 +0430

    Update(payment/models): add customer, log, authority fields to Payment

--- payment/models.py
+++ payment/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils.translation import gettext as _
 from accounts.models import Customer
 from address.models import CustomerAddress
+from cart.models import Cart
 from library.models import BaseModel
 import uuid
 
@@ -11,9 +12,9 @@ class Invoice(BaseModel):
                                  on_delete=models.SET_NULL, null=True)
     price = models.IntegerField(verbose_name=_('price'))
     is_paid = models.BooleanField(verbose_name=_('is paid'), default=False)
-    cart = models.OneToOneField(verbose_name=_('cart'), related_name='invoice', on_delete=models.PROTECT)
+    cart = models.OneToOneField(Cart, verbose_name=_('cart'), related_name='invoice', on_delete=models.PROTECT)
     address = models.ForeignKey(CustomerAddress, verbose_name=_('address'), related_name='invoices',
-                                on_delete=models.SET_NULL, null=True, blank=True)
+                                on_delete=models.SET_NULL, null=True)
 
     class Meta:
         verbose_name = _('Invoice')
@@ -26,9 +27,13 @@ class Invoice(BaseModel):
 
 class Payment(BaseModel):
     uuid = models.UUIDField(unique=True, verbose_name=_('uuid'), db_index=True, default=uuid.uuid4)
-    invoice = models.OneToOneField(Invoice, verbose_name=_('invoice'), related_name='payment', on_delete=models.CASCADE)
+    invoice = models.ForeignKey(Invoice, verbose_name=_('invoice'), related_name='payment', on_delete=models.CASCADE)
     price = models.IntegerField(verbose_name=_('price'))
+    customer = models.ForeignKey(Customer, verbose_name=_('customer'), related_name='payments',
+                                 on_delete=models.SET_NULL, null=True)
     is_paid = models.BooleanField(verbose_name=_('is paid'), default=False)
+    payment_log = models.TextField(verbose_name=_('logs'), blank=True)
+    authority = models.CharField(max_length=64, verbose_name=_('authority'), blank=True)
 
     class Meta:
         verbose_name = _('Payment')
