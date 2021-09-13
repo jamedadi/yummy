bae412e58c2651b0d2b4269cc3afee7b11d348bf
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-09-13 15:30:17 +0430

    Update(order/models): add class Meta, change invoice relation to 1to1

--- order/models.py
+++ order/models.py
@@ -13,6 +13,11 @@ class Order(BaseModel):
         (PREPARING_FOOD, _('preparing food')),
         (PREPARING_FOOD, _('sending')),
     )
-    invoice = models.ForeignKey(Invoice, verbose_name=_('invoice'), related_name='orders', on_delete=models.PROTECT)
-    status = models.PositiveSmallIntegerField(verbose_name=_('status'), choices=STATUS)
+    invoice = models.OneToOneField(Invoice, verbose_name=_('invoice'), related_name='order', on_delete=models.PROTECT)
+    status = models.PositiveSmallIntegerField(verbose_name=_('status'), choices=STATUS, default=PREPARING_FOOD)
     is_delivered = models.BooleanField(verbose_name=_('is delivered'), default=False)
+
+    class Meta:
+        verbose_name = _('Order')
+        verbose_name_plural = _('Orders')
+        db_table = 'order'
