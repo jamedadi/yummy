e3f708ab8db0a39013f5331a1e4fd810e276a294
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-09-13 15:54:56 +0430

    Update(order/models): add class Meta, customer 1to1 rel and create method

--- order/models.py
+++ order/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+
+from accounts.models import Customer
 from library.models import BaseModel
 from django.utils.translation import ugettext_lazy as _
 
@@ -14,6 +16,7 @@ class Order(BaseModel):
         (PREPARING_FOOD, _('sending')),
     )
     invoice = models.OneToOneField(Invoice, verbose_name=_('invoice'), related_name='order', on_delete=models.PROTECT)
+    customer = models.ForeignKey(Customer, verbose_name=_('customer'), related_name='orders', on_delete=models.PROTECT)
     status = models.PositiveSmallIntegerField(verbose_name=_('status'), choices=STATUS, default=PREPARING_FOOD)
     is_delivered = models.BooleanField(verbose_name=_('is delivered'), default=False)
 
@@ -21,3 +24,10 @@ class Order(BaseModel):
         verbose_name = _('Order')
         verbose_name_plural = _('Orders')
         db_table = 'order'
+
+    def __str__(self):
+        return f"{self.customer} - {self.status}"
+
+    @classmethod
+    def create(cls, invoice):
+        cls.objects.create(invoice=invoice, customer=invoice.customer)
