4adb887107b018875f04be0e3d22d03612175ba1
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-09-13 20:11:47 +0430

    Update(order/): remove is_delivered field and add it to status

--- order/models.py
+++ order/models.py
@@ -10,15 +10,15 @@ from payment.models import Invoice
 class Order(BaseModel):
     PREPARING_FOOD = 0
     SENDING = 1
-
+    DELIVERED = 2
     STATUS = (
         (PREPARING_FOOD, _('preparing food')),
-        (PREPARING_FOOD, _('sending')),
+        (SENDING, _('sending')),
+        (DELIVERED, _('delivered'))
     )
     invoice = models.OneToOneField(Invoice, verbose_name=_('invoice'), related_name='order', on_delete=models.PROTECT)
     customer = models.ForeignKey(Customer, verbose_name=_('customer'), related_name='orders', on_delete=models.PROTECT)
     status = models.PositiveSmallIntegerField(verbose_name=_('status'), choices=STATUS, default=PREPARING_FOOD)
-    is_delivered = models.BooleanField(verbose_name=_('is delivered'), default=False)
 
     class Meta:
         verbose_name = _('Order')
