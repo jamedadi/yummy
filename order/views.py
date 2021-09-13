99c2c63dba21ebcf2db6df58c313bd87aa723f7a
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-09-13 19:33:16 +0430

    Add(order/views): create CustomerOrderDetailView

--- order/views.py
+++ order/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.decorators.http import require_http_methods
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from accounts.models import Customer
+from accounts.utils import IsCustomer
+from library.utils import CustomUserPasses
+from order.models import Order
+
+
+@method_decorator(require_http_methods(['GET']), name='dispatch')
+@method_decorator(login_required(login_url=reverse_lazy('accounts:customer-login-register')), name='dispatch')
+class CustomerOrdersListView(IsCustomer, ListView):
+    template_name = 'order/customer/order_list.html'
+    context_object_name = 'orders'
+
+    def get_queryset(self):
+        return Order.objects.select_related('invoice__cart').filter(customer=self.request.user)
+
+
+@method_decorator(require_http_methods(['GET']), name='dispatch')
+@method_decorator(login_required(login_url=reverse_lazy('accounts:customer-login-register')), name='dispatch')
+class CustomerOrderDetailView(CustomUserPasses, DetailView):
+    model = Order
+    template_name = 'order/customer/order_detail.html'
+    context_object_name = 'order'
+
+    def test_func(self):
+        user = self.request.user
+        if not isinstance(user, Customer):
+            return False
+        if self.get_object().customer != self.request.user:
+            return False
+        return True
+
