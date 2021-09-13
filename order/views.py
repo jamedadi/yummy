734141afd352916593d1315f7f198e19118cc5ba
AmirHossein Bonakdar <amirhossein.bonakdar@yahoo.com>
2021-09-13 20:12:33 +0430

    Update(order/): seprate order list into active and delivered

--- order/views.py
+++ order/views.py
@@ -14,10 +14,16 @@ from order.models import Order
 @method_decorator(login_required(login_url=reverse_lazy('accounts:customer-login-register')), name='dispatch')
 class CustomerOrdersListView(IsCustomer, ListView):
     template_name = 'order/customer/order_list.html'
-    context_object_name = 'orders'
+    context_object_name = 'active_orders'
 
     def get_queryset(self):
-        return Order.objects.select_related('invoice__cart').filter(customer=self.request.user)
+        return Order.objects.select_related('invoice__cart').filter(customer=self.request.user, status__in=(0, 1))
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=None, **kwargs)
+        context['delivered_orders'] = Order.objects.select_related('invoice__cart').filter(customer=self.request.user,
+                                                                                           status=2)
+        return context
 
 
 @method_decorator(require_http_methods(['GET']), name='dispatch')
@@ -34,4 +40,3 @@ class CustomerOrderDetailView(CustomUserPasses, DetailView):
         if self.get_object().customer != self.request.user:
             return False
         return True
-
